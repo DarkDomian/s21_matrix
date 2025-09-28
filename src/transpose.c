81daed71132bd2b71ccb4ec091243e15f33b8a0b
trelawnm omsk <omsktrelawnm@le-e4.oms.21-school.ru>
2025-09-28 12:34:33 +0600

    feature(transpose): add new feature and unit tests for it

--- src/transpose.c
+++ src/transpose.c
@@ -3,9 +3,10 @@
 int s21_transpose(matrix_t *A, matrix_t *result) {
   int res = S21_SUCCESS;
 
-  if (!res) {
-    result->rows = A->rows;
-    result->columns = A->columns;
+  if (!(res = s21_create_matrix(A->columns, A->rows, result))) {
+    for (int i = 0; i < result->rows; ++i)
+      for (int j = 0; j < result->columns; ++j)
+        result->matrix[i][j] = A->matrix[j][i];
   }
 
   return res;
