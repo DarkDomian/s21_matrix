443c7f547619b28ea4b4f042505e1a20eb7fe25d
trelawnm omsk <omsktrelawnm@le-e4.oms.21-school.ru>
2025-09-29 17:22:02 +0600

    feature(inverse): finish with inverse matrix

--- src/inverse_matrix.c
+++ src/inverse_matrix.c
@@ -3,9 +3,22 @@
 int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
   int res = S21_SUCCESS;
 
+  double det = 0;
+  res = s21_determinant(A, &det);
+  if (!res && !det) res = S21_CALCERR;
+
   if (!res) {
-    result->rows = A->rows;
-    result->columns = A->columns;
+    matrix_t T;
+
+    s21_calc_complements(A, result);
+
+    s21_transpose(result, &T);
+
+    s21_remove_matrix(result);
+
+    s21_mult_number(&T, 1 / det, result);
+
+    s21_remove_matrix(&T);
   }
 
   return res;
--- tests/test_inverse_matrix.c
+++ tests/test_inverse_matrix.c
@@ -1,11 +1,40 @@
 #include "../include/s21_matrix.h"
 #include "../include/s21_suites.h"
 
-START_TEST(test_finish_success_inverse) {
+START_TEST(test_inverse_from_readme) {
   matrix_t A = {.rows = 0, .columns = 0, .matrix = NULL};
-  matrix_t B = {.rows = 0, .columns = 0, .matrix = NULL};
+  matrix_t B;
+  s21_create_matrix(3, 3, &A);
+
+  double digit_a[9] = {2, 5, 7, 6, 3, 4, 5, -2, -3};
+  double expected[9] = {1, -1, 1, -38, 41, -34, 27, -29, 24};
+
+  double* ptr_digit = digit_a;
+  for (int i = 0; i < A.rows * A.columns; ++i) A.matrix[0][i] = *ptr_digit++;
 
   ck_assert_int_eq(s21_inverse_matrix(&A, &B), S21_SUCCESS);
+
+  ptr_digit = expected;
+  for (int i = 0; i < B.rows * B.columns; ++i)
+    ck_assert_double_eq_tol(B.matrix[0][i], *ptr_digit++, S21_EPSILON);
+
+  s21_remove_matrix(&A);
+  s21_remove_matrix(&B);
+}
+END_TEST
+
+START_TEST(test_inverse_zero_determ) {
+  matrix_t A = {.rows = 0, .columns = 0, .matrix = NULL};
+  matrix_t B;
+  s21_create_matrix(2, 2, &A);
+
+  double digit_a[9] = {2, -7, -2, 7};
+
+  double* ptr_digit = digit_a;
+  for (int i = 0; i < A.rows * A.columns; ++i) A.matrix[0][i] = *ptr_digit++;
+
+  ck_assert_int_eq(s21_inverse_matrix(&A, &B), S21_CALCERR);
+
   s21_remove_matrix(&A);
 }
 END_TEST
@@ -14,7 +43,8 @@ Suite* test_inverse_matrix(void) {
   Suite* ps = suite_create("inverse");
   TCase* tc = tcase_create("core");
 
-  tcase_add_test(tc, test_finish_success_inverse);
+  tcase_add_test(tc, test_inverse_from_readme);
+  tcase_add_test(tc, test_inverse_zero_determ);
 
   suite_add_tcase(ps, tc);
   return ps;
