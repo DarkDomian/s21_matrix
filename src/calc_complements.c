fd3d8f1500a7bc5301354677827bc68da0d287d2
trelawnm omsk <omsktrelawnm@le-e4.oms.21-school.ru>
2025-09-29 16:34:40 +0600

    cofactor is ready. need more tests

--- src/calc_complements.c
+++ src/calc_complements.c
@@ -3,10 +3,21 @@
 int s21_calc_complements(matrix_t *A, matrix_t *result) {
   int res = S21_SUCCESS;
 
-  if (!res) {
-    result->rows = A->rows;
-    result->columns = A->columns;
+  if (!(res = s21_create_matrix(A->rows, A->rows, result))) {
+    for (int i = 0; i < A->rows && !res; ++i)
+      for (int j = 0; j < A->columns && !res; ++j) {
+        matrix_t minor;
+        res = _get_minor(A, i, j, &minor);
+        double det = 0;
+        if (!res) res = s21_determinant(&minor, &det);
+        result->matrix[i][j] = det;
+        s21_remove_matrix(&minor);
+      }
   }
+
+  for (int i = 0; i < result->rows && !res; ++i)
+    for (int j = 0; j < result->columns && !res; ++j)
+      result->matrix[i][j] *= ((i + j) % 2) ? -1.0 : 1.0;
 
   return res;
 }
--- tests/test_calc_complements.c
+++ tests/test_calc_complements.c
@@ -1,11 +1,25 @@
 #include "../include/s21_matrix.h"
 #include "../include/s21_suites.h"
 
-START_TEST(test_finish_success_compl) {
+START_TEST(test_calc_complements_v1) {
   matrix_t A = {.rows = 0, .columns = 0, .matrix = NULL};
-  matrix_t B = {.rows = 0, .columns = 0, .matrix = NULL};
+  matrix_t B;
+  s21_create_matrix(3, 3, &A);
+
+  double digit_a[9] = {1, 2, 3, 0, 4, 2, 5, 2, 1};
+  double expected[9] = {0, 10, -20, 4, -14, 8, -8, -2, 4};
+
+  double* ptr_digit = digit_a;
+  for (int i = 0; i < A.rows * A.columns; ++i) A.matrix[0][i] = *ptr_digit++;
 
   ck_assert_int_eq(s21_calc_complements(&A, &B), S21_SUCCESS);
+
+  ptr_digit = expected;
+  for (int i = 0; i < B.rows * B.columns; ++i)
+    ck_assert_double_eq_tol(B.matrix[0][i], *ptr_digit++, S21_EPSILON);
+
+  s21_remove_matrix(&A);
+  s21_remove_matrix(&B);
 }
 END_TEST
 
@@ -13,7 +27,7 @@ Suite* test_calc_complements(void) {
   Suite* ps = suite_create("complements");
   TCase* tc = tcase_create("core");
 
-  tcase_add_test(tc, test_finish_success_compl);
+  tcase_add_test(tc, test_calc_complements_v1);
 
   suite_add_tcase(ps, tc);
   return ps;
